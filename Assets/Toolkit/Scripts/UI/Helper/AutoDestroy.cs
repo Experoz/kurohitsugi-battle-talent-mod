8cae0063b0f7d620b048818536c8b2c857e6d32c
Experoz <ezio.perini@outlook.it>
2026-05-12 22:14:45 +0200

    Initial working Kurohitsugi sequence

---
+++ Assets/Toolkit/Scripts/UI/Helper/AutoDestroy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CrossLink
+{
+    public class AutoDestroy : MonoBehaviour
+    {
+        public float destroyDelay = 3f;
+
+#if UNITY_EDITOR
+        bool isActive = false;
+        float currentTime = 0f;
+        public void StartTimer()
+        {
+            isActive = true;
+        }
+
+        private void Update()
+        {
+            if (!isActive) return;
+
+            currentTime += Time.deltaTime;
+
+            if (currentTime >= destroyDelay)
+            {
+                Destroy(gameObject);
+            }
+        }
+#endif
+    }
+
+}
